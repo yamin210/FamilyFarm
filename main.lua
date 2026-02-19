b7c122a62a7ed4fa3f9f9f8a97cc0f4c21d80780
yamin210 <132365837+yamin210@users.noreply.github.com>
2026-02-19 19:11:07 +0100

    Update toast messages for process notifications

--- main.lua
+++ main.lua
@@ -48,7 +48,7 @@ function alleMaschinenHack()
     end
 
     gg.sleep(200) -- Kurze Pause für die Stabilität
-    gg.toast("⏳ جاري العمل... (Prozess 2)")
+    gg.toast("يلا هاا رح اخلص")
 
     -- --- ZWEITE SUCHE ---
     gg.clearResults()
@@ -66,7 +66,7 @@ function alleMaschinenHack()
                 gg.setValues(found)
             end
         end
-        gg.toast("✅ Prozess 2 abgeschlossen")
+        gg.toast("خلصناااا")
     else
         gg.toast("❌ Gruppe 2 nicht gefunden")
     end
