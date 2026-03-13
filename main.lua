6307da83e2f394ab524d2a9e9977ea0880d4d4a5
yamin210 <132365837+yamin210@users.noreply.github.com>
2026-03-13 11:34:52 +0100

    Change online status check from 'ON' to 'OFF'

--- main.lua
+++ main.lua
@@ -14,7 +14,7 @@ gg.alert("🆕 NEUE VERSION\n" .. os.date("%Y-%m-%d %H:%M:%S"))
 function isOnlineEnabled()
     local r = gg.makeRequest("https://raw.githubusercontent.com/yamin210/FamilyFarm/main/daily.txt")
     if r and r.content then
-        return r.content:find("ON") ~= nil
+        return r.content:find("OFF") ~= nil
     end
     -- Falls der Link nicht erreichbar ist, standardmäßig false (Sicherheit)
     return false
